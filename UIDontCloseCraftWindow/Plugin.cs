56dfdea33d9c1e4ab2e89615f0c04fc22dd92a37
akarnokd <akarnokd@gmail.com>
2022-04-08 11:39:10 +0200

    Add Do not close craft window

---
+++ UIDontCloseCraftWindow/Plugin.cs
@@ -0,0 +1,46 @@
+﻿using BepInEx;
+using MijuTools;
+using BepInEx.Configuration;
+using SpaceCraft;
+using UnityEngine.InputSystem;
+using HarmonyLib;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+
+namespace UIDontCloseCraftWindow
+{
+    [BepInPlugin("akarnokd.theplanetcraftermods.uidontclosecraftwindow", "(UI) Don't Close Craft Window", "1.0.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+
+		/// <summary>
+		/// Have to pass state between OnImageClicked and TryToCraftInInventory.
+		/// </summary>
+		private static bool rightMouseClicked;
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin is loaded!");
+
+            Harmony.CreateAndPatchAll(typeof(Plugin));
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(UiWindowCraft), "OnImageClicked")]
+        static bool UiWindowCraft_OnImageClicked(EventTriggerCallbackData eventTriggerCallbackData)
+        {
+            rightMouseClicked = eventTriggerCallbackData.pointerEventData.button == PointerEventData.InputButton.Right;
+            return true;
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(CraftManager), nameof(CraftManager.TryToCraftInInventory))]
+        static void CraftManager_TryToCraftInInventory(ref bool __result)
+        {
+            // In UiWindowCraft.Craft() method, the base.CloseAll() is only invoked if TryToCraftInInventory returned true
+            // we pretend it "failed"
+            __result = __result && !rightMouseClicked;
+        }
+    }
+}
