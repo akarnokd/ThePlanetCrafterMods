8406a2bdd8cc4b48c466fc758cf28f59b316f13c
akarnokd <akarnokd@gmail.com>
2022-04-08 11:20:33 +0200

    Add UI Inventory Move multiple

---
+++ UIInventoryMoveMultiple/Plugin.cs
@@ -0,0 +1,88 @@
+﻿using BepInEx;
+using MijuTools;
+using BepInEx.Configuration;
+using SpaceCraft;
+using UnityEngine.InputSystem;
+using HarmonyLib;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+
+namespace UIInventoryMoveMultiple
+{
+    [BepInPlugin("akarnokd.theplanetcraftermods.uiinventorymovemultiple", "(UI) Inventory Move Multiple Items", "1.0.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+
+        /// <summary>
+        /// How many items to move when only a few to move
+        /// </summary>
+        static int moveFew;
+        /// <summary>
+        /// How many items to move when many to move.
+        /// </summary>
+        static int moveMany;
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin is loaded!");
+
+            moveFew = Config.Bind<int>("General", "MoveFewAmount", 5, "How many items to move when only a few to move.").Value;
+            moveMany = Config.Bind<int>("General", "MoveManyAmount", 50, "How many items to move when many to move.").Value;
+
+            Harmony.CreateAndPatchAll(typeof(Plugin));
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(InventoryDisplayer), "OnImageClicked")]
+        static bool InventoryDisplayer_OnImageClicked(EventTriggerCallbackData _eventTriggerCallbackData, Inventory ___inventory)
+        {
+			if (_eventTriggerCallbackData.pointerEventData.button == PointerEventData.InputButton.Middle)
+			{
+				int max = int.MaxValue;
+				if (Keyboard.current[Key.LeftShift].isPressed)
+				{
+					if (Keyboard.current[Key.LeftCtrl].isPressed)
+					{
+						max = moveMany;
+					}
+					else
+					{
+						max = moveFew;
+					}
+				}
+				DataConfig.UiType openedUi = Managers.GetManager<WindowsHandler>().GetOpenedUi();
+				if (openedUi == DataConfig.UiType.Container)
+				{
+					Inventory otherInventory = ((UiWindowContainer)Managers.GetManager<WindowsHandler>().GetWindowViaUiId(openedUi)).GetOtherInventory(___inventory);
+					if (___inventory != null && otherInventory != null)
+					{
+						string id = _eventTriggerCallbackData.worldObject.GetGroup().GetId();
+						List<WorldObject> list = new List<WorldObject>();
+						foreach (WorldObject worldObject in ___inventory.GetInsideWorldObjects())
+						{
+							if (worldObject.GetGroup().GetId() == id)
+							{
+								list.Add(worldObject);
+							}
+						}
+						int c = 0;
+						foreach (WorldObject worldObject2 in list)
+						{
+							if (otherInventory.AddItem(worldObject2))
+							{
+								___inventory.RemoveItem(worldObject2, false);
+								if (++c >= max)
+								{
+									break;
+								}
+							}
+						}
+						Managers.GetManager<BaseHudHandler>().DisplayCursorText("", 3f, "Transfer " + id + " x " + c);
+					}
+				}
+			}
+			return true;
+        }
+    }
+}
