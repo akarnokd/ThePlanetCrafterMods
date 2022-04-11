15a875a07067d8d2184952765ebbc988aaebee9a
Dávid Karnok <akarnokd@gmail.com>
2022-04-11 19:50:04 +0200

    Save When Quitting mod

---
+++ UISaveOnQuit/Plugin.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using MijuTools;
+using BepInEx.Configuration;
+using SpaceCraft;
+using UnityEngine.InputSystem;
+using HarmonyLib;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+
+namespace UISaveOnQuit
+{
+    [BepInPlugin("akarnokd.theplanetcraftermods.saveonquit", "(UI) Save When Quitting", "1.0.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin is loaded!");
+
+            Harmony.CreateAndPatchAll(typeof(Plugin));
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(UiWindowPause), nameof(UiWindowPause.OnQuit))]
+        static void UiWindowPause_OnQuit(UiWindowPause __instance)
+        {
+            __instance.OnSave();
+        }
+    }
+}
