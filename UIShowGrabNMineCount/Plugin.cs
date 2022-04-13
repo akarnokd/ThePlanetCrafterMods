023e9792c2aa5763d31ca9998607e99c7c8467bb
akarnokd <akarnokd@gmail.com>
2022-04-13 11:21:11 +0200

    Add Mod ShowGrabNMineCount

---
+++ UIShowGrabNMineCount/Plugin.cs
@@ -0,0 +1,100 @@
+﻿using BepInEx;
+using SpaceCraft;
+using HarmonyLib;
+using UnityEngine;
+using MijuTools;
+using UnityEngine.InputSystem;
+using BepInEx.Configuration;
+
+namespace UIShowGrabNMineCount
+{
+    [BepInPlugin("akarnokd.theplanetcraftermods.uishowgrabnminecount", "(UI) Show Grab N Mine Count", "1.0.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private static ConfigEntry<bool> isEnabled;
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin is loaded!");
+
+            isEnabled = Config.Bind("General", "Enabled", true, "Is the visual notification enabled?");
+
+            Harmony.CreateAndPatchAll(typeof(Plugin));
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(ActionMinable), "FinishMining")]
+        static bool ActionMinable_FinishMining(ActionMinable __instance, 
+            PlayerMainController ___playerSource,
+            ref bool ___firstReduceDelay, PlayerAnimations ___playerAnimations,
+            ItemWorldDislpayer ___itemWorldDisplayer, float ___timeMineStarted, float ___timeMineStoped) 
+        {
+            __instance.StopAllCoroutines();
+            if (___timeMineStarted - ___timeMineStoped > ___playerSource.GetMultitool().GetMultiToolMine().GetMineTime())
+            {
+                // This also should fix two potential NPEs with quickly disappearing ores
+                WorldObjectAssociated woa = __instance.GetComponent<WorldObjectAssociated>();
+                if (woa != null) 
+                {
+                    WorldObject worldObject = woa.GetWorldObject();
+                    if (worldObject != null) 
+                    {
+                        worldObject.SetDontSaveMe(false);
+
+                        ___playerSource.GetMultitool().GetMultiToolMine().Mine(false, true);
+                        ___playerSource.GetPlayerBackpack().GetInventory().AddItem(worldObject);
+                        ___playerAnimations.AnimateRecolt(false);
+                        ___itemWorldDisplayer.Hide();
+                        ___firstReduceDelay = false;
+                        UnityEngine.Object.Destroy(__instance.gameObject);
+
+                        if (isEnabled.Value)
+                        {
+                            ShowInventoryAdded(worldObject, ___playerSource.GetPlayerBackpack().GetInventory());
+                        }
+                     }
+                }
+            }
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(ActionGrabable), "Grab")]
+        static bool ActionGrabable_Grab(ActionGrabable __instance, PlayerMainController ___playerSource)
+        {
+            if (isEnabled.Value)
+            {
+                WorldObjectAssociated woa = __instance.GetComponent<WorldObjectAssociated>();
+                if (woa != null)
+                {
+                    WorldObject worldObject = woa.GetWorldObject();
+                    if (worldObject != null)
+                    {
+                        ShowInventoryAdded(worldObject, ___playerSource.GetPlayerBackpack().GetInventory());
+                    }
+                }
+            }
+            return true;
+        }
+
+        static void ShowInventoryAdded(WorldObject worldObject, Inventory inventory)
+        {
+            int c = 0;
+            Group group = worldObject.GetGroup();
+            string gid = group.GetId();
+            foreach (WorldObject wo in inventory.GetInsideWorldObjects())
+            {
+                if (wo.GetGroup().GetId() == gid)
+                {
+                    c++;
+                }
+            }
+
+            string text = Readable.GetGroupName(group) + " + 1  (  " + c + "  )";
+            InformationsDisplayer informationsDisplayer = Managers.GetManager<DisplayersHandler>().GetInformationsDisplayer();
+            informationsDisplayer.AddInformation(2f, text, DataConfig.UiInformationsType.InInventory, group.GetImage());
+        }
+
+    }
+}
