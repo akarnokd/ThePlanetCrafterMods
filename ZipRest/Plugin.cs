5ef3169b5e39ebd65181698f74d3de65344b23a3
akarnokd <akarnokd@gmail.com>
2022-04-15 17:54:31 +0200

    Zip all plugins

---
+++ ZipRest/Plugin.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using UnityEngine;
+using BepInEx.Bootstrap;
+using System.Reflection;
+using System;
+using HarmonyLib;
+using SpaceCraft;
+using System.Collections.Generic;
+using BepInEx.Logging;
+using MijuTools;
+using System.Text;
+
+namespace ZipRest
+{
+    [BepInPlugin("akarnokd.theplanetcraftermods.ziprest", "Zip the other mods", "1.0.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin is loaded!");
+
+            // Harmony.CreateAndPatchAll(typeof(Plugin));
+        }
+    }
+}
