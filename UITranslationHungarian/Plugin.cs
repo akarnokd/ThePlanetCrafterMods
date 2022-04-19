1ad1b7054ef58c7627764c6f6bc38fd31d560efa
akarnokd <akarnokd@gmail.com>
2022-04-19 16:08:30 +0200

    Apróbb fordítási javítások

--- UITranslationHungarian/Plugin.cs
+++ UITranslationHungarian/Plugin.cs
@@ -14,7 +14,7 @@ using BepInEx.Configuration;
 
 namespace UITranslationHungarian
 {
-    [BepInPlugin("akarnokd.theplanetcraftermods.uitranslationhungarian", "(UI) Hungarian Translation", "1.0.0.0")]
+    [BepInPlugin("akarnokd.theplanetcraftermods.uitranslationhungarian", "(UI) Hungarian Translation", "1.0.0.1")]
     public class Plugin : BaseUnityPlugin
     {
         const string languageKey = "hungarian";
