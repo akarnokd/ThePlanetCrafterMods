936b6a6da3dd81501703c52f0e0537680fc41609
Dávid Karnok <akarnokd@gmail.com>
2022-04-21 20:07:12 +0200

    Fix wording

--- UITranslationHungarian/Plugin.cs
+++ UITranslationHungarian/Plugin.cs
@@ -14,7 +14,7 @@ using BepInEx.Configuration;
 
 namespace UITranslationHungarian
 {
-    [BepInPlugin("akarnokd.theplanetcraftermods.uitranslationhungarian", "(UI) Hungarian Translation", "1.0.0.3")]
+    [BepInPlugin("akarnokd.theplanetcraftermods.uitranslationhungarian", "(UI) Hungarian Translation", "1.0.0.4")]
     public class Plugin : BaseUnityPlugin
     {
         const string languageKey = "hungarian";
