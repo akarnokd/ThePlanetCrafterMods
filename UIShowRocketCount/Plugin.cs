91ad325b615b098be0cc1a9173ee4d725fdb1eff
WildCard65 <lollol222gg@gmail.com>
2022-05-24 14:59:18 -0400

    Reworked the plugin for better overall performance.
    This plugin will no longer constantly iterate the game for rockets, instead it'll do a one time iteration for the relevant inventories, creating them prematurely if needed.

--- UIShowRocketCount/Plugin.cs
+++ UIShowRocketCount/Plugin.cs
@@ -9,6 +9,9 @@ using UnityEngine.UI;
 using System.Reflection;
 using BepInEx.Configuration;
 using BepInEx.Logging;
+using System.Reflection.Emit;
+using System.Text;
+using System.Linq;
 
 namespace UIShowRocketCount
 {
@@ -18,6 +21,15 @@ namespace UIShowRocketCount
         static FieldInfo associatedGroup;
         static ConfigEntry<int> fontSize;
         static ManualLogSource logger;
+
+        static DataConfig.WorldUnitType[] rocketTypes = new DataConfig.WorldUnitType[]
+        {
+            DataConfig.WorldUnitType.Oxygen,
+            DataConfig.WorldUnitType.Heat,
+            DataConfig.WorldUnitType.Pressure,
+            DataConfig.WorldUnitType.Biomass,
+        };
+        static Dictionary<DataConfig.WorldUnitType, int> rocketsCount = new Dictionary<DataConfig.WorldUnitType, int>();
 
         private void Awake()
         {
@@ -32,83 +44,112 @@ namespace UIShowRocketCount
             Harmony.CreateAndPatchAll(typeof(Plugin));
         }
 
-        [HarmonyPrefix]
-        [HarmonyPatch(typeof(WorldUnitsGenerationDisplayer), "RefreshDisplay")]
-
-        static bool WorldUnitsGenerationDisplayer_RefreshDisplay(ref IEnumerator __result, float timeRepeat,
-            WorldUnitsHandler ___worldUnitsHandler, List<TextMeshProUGUI> ___textFields, List<DataConfig.WorldUnitType> ___correspondingUnit)
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(SessionController), "Start")]
+        public static void GetAllRockets()
         {
-            __result = RefreshDisplayEnumerator(timeRepeat, ___worldUnitsHandler, ___textFields, ___correspondingUnit);
-            return false;
-        }
-
-        static void CountRockets(Dictionary<DataConfig.WorldUnitType, int> rockets)
-        {
-            foreach (WorldObject worldObject in WorldObjectsHandler.GetAllWorldObjects())
+            List<WorldObject> constructedWorldObjects = WorldObjectsHandler.GetConstructedWorldObjects();
+            foreach (DataConfig.WorldUnitType unitType in rocketTypes)
             {
-                string gid = worldObject.GetGroup().GetId();
+                if (GameConfig.spaceGlobalMultipliersGroupIds.ContainsKey(unitType))
+                {
+                    Group unitGroup = GroupsHandler.GetGroupViaId(GameConfig.spaceGlobalMultipliersGroupIds[unitType]);
+                    if (unitGroup != null)
+                    {
+                        WorldObject unitStorage = null;
+                        foreach (WorldObject worldObject in constructedWorldObjects)
+                        {
+                            if (worldObject.GetGroup() == unitGroup)
+                            {
+                                unitStorage = worldObject;
+                                break;
+                            }
+                        }
 
-                if (gid.StartsWith("RocketOxygen"))
-                {
-                    rockets.TryGetValue(DataConfig.WorldUnitType.Oxygen, out int v);
-                    rockets[DataConfig.WorldUnitType.Oxygen] = v + 1;
-                } else
-                if (gid.StartsWith("RocketHeat"))
-                {
-                    rockets.TryGetValue(DataConfig.WorldUnitType.Heat, out int v2);
-                    rockets[DataConfig.WorldUnitType.Heat] = v2 + 1;
-                } else
-                if (gid.StartsWith("RocketPressure"))
-                {
-                    rockets.TryGetValue(DataConfig.WorldUnitType.Pressure, out int v3);
-                    rockets[DataConfig.WorldUnitType.Pressure] = v3 + 1;
-                } else
-                if (gid.StartsWith("RocketBiomass"))
-                {
-                    rockets.TryGetValue(DataConfig.WorldUnitType.Biomass, out int v4);
-                    rockets[DataConfig.WorldUnitType.Biomass] = v4 + 1;
+                        if (unitStorage == null)
+                        {
+                            // This is the same code the game uses, so theoretically it should not break anything.
+                            unitStorage = WorldObjectsHandler.CreateNewWorldObject(unitGroup, 0);
+                            unitStorage.SetPositionAndRotation(GameConfig.spaceLocation, Quaternion.identity);
+
+                            WorldObjectsHandler.InstantiateWorldObject(unitStorage, false);
+                        }
+
+                        Inventory rocketInventory = InventoriesHandler.GetInventoryById(unitStorage.GetLinkedInventoryId());
+                        if (rocketInventory != null)
+                        {
+                            int rocketCount = rocketInventory.GetInsideWorldObjects().Where(worldObject => worldObject.GetGroup().GetId().StartsWith($"Rocket{unitType:G}")).Count();
+                            rocketsCount[unitType] = rocketCount;
+
+                            logger.LogInfo($"We found {rocketCount} of {unitType:G} rockets.");
+                            logger.LogDebug($"Current rocket count is: {rocketsCount[unitType]}");
+
+                            rocketInventory.inventoryContentModified += RocketInventoryModified;
+                        }
+                    }
                 }
             }
         }
 
-        static void CountRockets(Dictionary<string, int> rockets)
+        private static void RocketInventoryModified(WorldObject _worldObjectModified, bool _added)
         {
-            foreach (WorldObject worldObject in WorldObjectsHandler.GetAllWorldObjects())
+            foreach (DataConfig.WorldUnitType unitType in rocketTypes)
             {
-                string gid = worldObject.GetGroup().GetId();
-
-                if (gid.StartsWith("Rocket"))
+                if (_worldObjectModified.GetGroup().GetId().StartsWith($"Rocket{unitType:G}"))
                 {
-                    rockets.TryGetValue(gid, out int v);
-                    rockets[gid] = v + 1;
+                    int countChange = (_added ? 1 : -1);
+                    rocketsCount[unitType] += countChange;
+
+                    logger.LogInfo($"Rocket count for {unitType:G} has changed by {countChange}.");
                 }
             }
         }
 
-        static IEnumerator RefreshDisplayEnumerator(float timeRepeat, WorldUnitsHandler worldUnitsHandler, 
-            List<TextMeshProUGUI> textFields, List<DataConfig.WorldUnitType> correspondingUnit)
+        [HarmonyPriority(Priority.First)]
+        [HarmonyTranspiler]
+        [HarmonyPatch(typeof(WorldUnitsDisplayer), "RefreshDisplay")]
+        static IEnumerable<CodeInstruction> JumpOut_RefreshDisplay(IEnumerable<CodeInstruction> instructions)
+        {
+            MethodInfo targetMethod = typeof(Plugin).GetMethod(nameof(RefreshDisplay_Modded), BindingFlags.Static | BindingFlags.Public);
+
+            FieldInfo worldUnitsHandler = AccessTools.Field(typeof(WorldUnitsDisplayer), "worldUnitsHandler");
+            FieldInfo correspondingUnit = AccessTools.Field(typeof(WorldUnitsDisplayer), "correspondingUnit");
+            FieldInfo textFields = AccessTools.Field(typeof(WorldUnitsDisplayer), "textFields");
+
+            yield return new CodeInstruction(OpCodes.Ldarg_1);
+            yield return new CodeInstruction(OpCodes.Ldarg_0);
+            yield return new CodeInstruction(OpCodes.Ldfld, worldUnitsHandler);
+            yield return new CodeInstruction(OpCodes.Ldarg_0);
+            yield return new CodeInstruction(OpCodes.Ldfld, correspondingUnit);
+            yield return new CodeInstruction(OpCodes.Ldarg_0);
+            yield return new CodeInstruction(OpCodes.Ldfld, textFields);
+            yield return new CodeInstruction(OpCodes.Call, targetMethod);
+            yield return new CodeInstruction(OpCodes.Ret);
+        }
+
+        public static IEnumerator RefreshDisplay_Modded(float timeRepeat, WorldUnitsHandler worldUnitsHandler, List<DataConfig.WorldUnitType> correspondingUnit, List<TextMeshProUGUI> textFields)
         {
             for (; ; )
             {
                 if (worldUnitsHandler != null)
                 {
-                    Dictionary<DataConfig.WorldUnitType, int> rockets = new Dictionary<DataConfig.WorldUnitType, int>();
-                    CountRockets(rockets);
-                    for (int idx = 0; idx < textFields.Count; idx++)
+                    for (int index = 0; index < textFields.Count; ++index)
                     {
-                        WorldUnit unit = worldUnitsHandler.GetUnit(correspondingUnit[idx]);
+                        WorldUnit unit = worldUnitsHandler.GetUnit(correspondingUnit[index]);
                         if (unit != null)
                         {
-                            string s = unit.GetDisplayStringForValue(unit.GetCurrentValuePersSec(), false, 0) + "/s";
-                            rockets.TryGetValue(unit.GetUnitType(), out int c);
-                            if (c > 0)
-                            {
-                                s = c + " x -----    " + s;
-                            }
-                            textFields[idx].text = s;
+                            StringBuilder displayText = new StringBuilder();
+
+                            if (rocketsCount.TryGetValue(unit.GetUnitType(), out int rocketCount))
+                                displayText.AppendFormat("{0} x -----    ", rocketCount);
+
+                            displayText.Append(unit.GetValueString());
+
+                            textFields[index].text = displayText.ToString();
                         }
                     }
                 }
+
                 yield return new WaitForSeconds(timeRepeat);
             }
         }
@@ -117,8 +158,6 @@ namespace UIShowRocketCount
         [HarmonyPatch(typeof(UiWindowCraft), "CreateGrid")]
         static void UiWindowCraft_CreateGrid(GridLayoutGroup ___grid)
         {
-            Dictionary<string, int> rockets = new Dictionary<string, int>();
-            CountRockets(rockets);
             int fs = fontSize.Value;
 
             foreach (Transform tr in ___grid.transform)
@@ -129,28 +168,35 @@ namespace UIShowRocketCount
                     Group g = associatedGroup.GetValue(ehg) as Group;
                     if (g != null)
                     {
-                        if (rockets.TryGetValue(g.GetId(), out int c)) {
-                            if (c > 0)
+                        foreach (DataConfig.WorldUnitType unitType in rocketTypes)
+                        {
+                            if (g.GetId().StartsWith($"Rocket{unitType:G}"))
                             {
-                                GameObject go = new GameObject();
-                                Transform parent = tr.gameObject.transform;
-                                go.transform.parent = parent;
+                                int rocketCount = rocketsCount[unitType];
+                                if (rocketCount > 0)
+                                {
+                                    GameObject go = new GameObject();
+                                    Transform parent = tr.gameObject.transform;
+                                    go.transform.parent = parent;
 
-                                Text text = go.AddComponent<Text>();
-                                text.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
-                                text.text = c + " x";
-                                text.color = new Color(1f, 1f, 1f, 1f);
-                                text.fontSize = fs;
-                                text.resizeTextForBestFit = false;
-                                text.verticalOverflow = VerticalWrapMode.Truncate;
-                                text.horizontalOverflow = HorizontalWrapMode.Overflow;
-                                text.alignment = TextAnchor.MiddleCenter;
+                                    Text text = go.AddComponent<Text>();
+                                    text.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
+                                    text.text = rocketCount + " x";
+                                    text.color = new Color(1f, 1f, 1f, 1f);
+                                    text.fontSize = fs;
+                                    text.resizeTextForBestFit = false;
+                                    text.verticalOverflow = VerticalWrapMode.Truncate;
+                                    text.horizontalOverflow = HorizontalWrapMode.Overflow;
+                                    text.alignment = TextAnchor.MiddleCenter;
 
-                                Vector2 v = tr.gameObject.GetComponent<Image>().GetComponent<RectTransform>().sizeDelta;
+                                    Vector2 v = tr.gameObject.GetComponent<Image>().GetComponent<RectTransform>().sizeDelta;
 
-                                RectTransform rectTransform = text.GetComponent<RectTransform>();
-                                rectTransform.localPosition = new Vector3(0, v.y / 2 + 10, 0);
-                                rectTransform.sizeDelta = new Vector2(fs * 3, fs + 5);
+                                    RectTransform rectTransform = text.GetComponent<RectTransform>();
+                                    rectTransform.localPosition = new Vector3(0, v.y / 2 + 10, 0);
+                                    rectTransform.sizeDelta = new Vector2(fs * 3, fs + 5);
+                                }
+
+                                break;
                             }
                         }
                     }
