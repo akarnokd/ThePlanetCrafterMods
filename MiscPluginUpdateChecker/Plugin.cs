ec43cf879a20844c6556ccfa74712236b8f64f5f
akarnokd <akarnokd@gmail.com>
2022-06-03 17:01:04 +0200

    Compare local and remote

--- MiscPluginUpdateChecker/Plugin.cs
+++ MiscPluginUpdateChecker/Plugin.cs
@@ -11,6 +11,9 @@ using System;
 using System.Net;
 using System.IO;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using BepInEx.Bootstrap;
 
 [assembly: InternalsVisibleTo("XTestPlugins")]
 namespace MiscPluginUpdateChecker
@@ -38,12 +41,83 @@ namespace MiscPluginUpdateChecker
             Harmony.CreateAndPatchAll(typeof(Plugin));
         }
 
+        static CancellationTokenSource cancelDownload;
+        static volatile Dictionary<string, PluginEntry> pluginInfos;
+
         [HarmonyPostfix]
         [HarmonyPatch(typeof(Intro), "Start")]
         static void Intro_Start()
         {
+            if (isEnabled.Value)
+            {
+                pluginInfos = null;
 
+                var startUrl = versionInfoRepository.Value;
+                var bypass = bypassCache.Value;
+
+                var localPlugins = GetLocalPlugins();
+
+                cancelDownload = new CancellationTokenSource();
+                Task.Factory.StartNew(() => GetPluginInfos(startUrl, bypass, localPlugins),
+                    cancelDownload.Token,
+                    TaskCreationOptions.LongRunning,
+                    TaskScheduler.Default);
+            }
         }
 
+        static void Update()
+        {
+            if (!cancelDownload.IsCancellationRequested)
+            {
+                var pis = pluginInfos;
+                if (pis != null)
+                {
+
+                }
+            }
+        }
+
+        static void Destroy()
+        {
+            cancelDownload?.Cancel();
+            pluginInfos = null;
+        }
+
+        static Dictionary<string, PluginEntry> GetLocalPlugins()
+        {
+            var result = new Dictionary<string, PluginEntry>();
+            foreach (var pi in Chainloader.PluginInfos)
+            {
+                var pe = new PluginEntry();
+                pe.guid = pi.Key;
+                pe.description = pi.Value.Metadata.Name;
+                pe.explicitVersion = pi.Value.Metadata.Version.ToString();
+
+                result[pi.Key] = pe;
+            }
+            return result;
+        }
+
+        static void GetPluginInfos(string startUrl, bool bypassCache, Dictionary<string, PluginEntry> localPlugins)
+        {
+            try
+            {
+                var remotePluginInfos = Helpers.DownloadPluginInfos(
+                    startUrl,
+                    o => logger.LogInfo(o),
+                    o => logger.LogWarning(o),
+                    o => logger.LogError(o),
+                    bypassCache
+                );
+                logger.LogInfo("Comparing local and remote plugins");
+            }
+            catch (Exception ex)
+            {
+                if (!cancelDownload.IsCancellationRequested)
+                {
+                    logger.LogError(ex);
+                }
+            }
+        }
     }
 }
