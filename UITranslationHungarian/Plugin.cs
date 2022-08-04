e803da4a3fd77bd7749617e7c70afeda9cb71faf
Dávid Karnok <akarnokd@gmail.com>
2022-08-04 15:39:06 +0200

    Hun: added missing translation

--- UITranslationHungarian/Plugin.cs
+++ UITranslationHungarian/Plugin.cs
@@ -13,7 +13,7 @@ using System.Collections;
 
 namespace UITranslationHungarian
 {
-    [BepInPlugin("akarnokd.theplanetcraftermods.uitranslationhungarian", "(UI) Hungarian Translation", "1.0.0.11")]
+    [BepInPlugin("akarnokd.theplanetcraftermods.uitranslationhungarian", "(UI) Hungarian Translation", "1.0.0.12")]
     public class Plugin : BaseUnityPlugin
     {
         const string languageKey = "hungarian";
