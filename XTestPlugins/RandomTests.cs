193a27ada785de916171963c09f150092dea3231
Dávid Karnok <akarnokd@gmail.com>
2022-10-13 23:40:27 +0200

    Some test

---
+++ XTestPlugins/RandomTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XTestPlugins
+{
+    [TestClass]
+    public class RandomTests
+    {
+        [TestMethod]
+        public void IsModded()
+        {
+            Assert.IsTrue(Directory.GetFileSystemEntries("E:\\Steam\\steamapps\\common\\The Planet Crafter\\BepInEx\\plugins\\").Length != 0);
+        }
+    }
+}
