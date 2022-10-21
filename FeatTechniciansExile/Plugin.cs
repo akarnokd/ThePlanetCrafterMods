25f5603dcece5ed9df9bfc4b7fa4fd8a37147b20
akarnokd <akarnokd@gmail.com>
2022-10-22 00:13:17 +0200

    Improve technician's cabin

--- FeatTechniciansExile/Plugin.cs
+++ FeatTechniciansExile/Plugin.cs
@@ -32,6 +32,7 @@ namespace FeatTechniciansExile
 
         static Func<string> multiplayerMode;
 
+        // /tp 1990 76 1073
         static Vector3 technicianDropLocation = new Vector3(1995, 75.7f, 1073);
 
         static TechnicianAvatar avatar;
@@ -45,6 +46,10 @@ namespace FeatTechniciansExile
         static WorldObject screen;
         static WorldObject grower;
         static WorldObject collector;
+        static WorldObject flower;
+        static WorldObject chair;
+        static WorldObject chest;
+        static WorldObject solar;
 
         private void Awake()
         {
@@ -117,11 +122,12 @@ namespace FeatTechniciansExile
                 logger.LogInfo("    Creating the Bed");
                 bed = WorldObjectsHandler.CreateNewWorldObject(GroupsHandler.GetGroupViaId("BedSimple"), 
                     technicianWorldObjectIdStart + 2);
-                bed.SetPositionAndRotation(livingPodFloor, Quaternion.identity);
+                bed.SetPositionAndRotation(livingPodFloor + new Vector3(3, 0, -1), Quaternion.identity * Quaternion.Euler(0, 180, 0));
                 bed.SetDontSaveMe(dontSaveMe);
                 WorldObjectsHandler.InstantiateWorldObject(bed, false);
             }
 
+            var deskPosition = livingPodFloor + new Vector3(-1, 0, -3);
             logger.LogInfo("  Finding the Desk");
             desk = WorldObjectsHandler.GetWorldObjectViaId(technicianWorldObjectIdStart + 3);
             if (desk == null)
@@ -129,7 +135,7 @@ namespace FeatTechniciansExile
                 logger.LogInfo("    Creating the Desk");
                 desk = WorldObjectsHandler.CreateNewWorldObject(GroupsHandler.GetGroupViaId("Desktop1"), 
                     technicianWorldObjectIdStart + 3);
-                desk.SetPositionAndRotation(livingPodFloor, Quaternion.identity);
+                desk.SetPositionAndRotation(deskPosition, Quaternion.identity * Quaternion.Euler(0, -90, 0));
                 desk.SetDontSaveMe(dontSaveMe);
                 WorldObjectsHandler.InstantiateWorldObject(desk, false);
             }
@@ -141,7 +147,7 @@ namespace FeatTechniciansExile
                 logger.LogInfo("    Creating the Rockets Screen");
                 screen = WorldObjectsHandler.CreateNewWorldObject(GroupsHandler.GetGroupViaId("ScreenRockets"),
                     technicianWorldObjectIdStart + 4);
-                screen.SetPositionAndRotation(livingPodFloor, Quaternion.identity);
+                screen.SetPositionAndRotation(deskPosition + new Vector3(-0.5f, 1f, 0.25f), Quaternion.identity * Quaternion.Euler(0, 180, 0));
                 screen.SetDontSaveMe(dontSaveMe);
                 WorldObjectsHandler.InstantiateWorldObject(screen, false);
             }
@@ -153,7 +159,7 @@ namespace FeatTechniciansExile
                 logger.LogInfo("    Creating the Grower");
                 grower = WorldObjectsHandler.CreateNewWorldObject(GroupsHandler.GetGroupViaId("VegetableGrower1"),
                     technicianWorldObjectIdStart + 5);
-                grower.SetPositionAndRotation(livingPodFloor, Quaternion.identity);
+                grower.SetPositionAndRotation(livingPodFloor + new Vector3(2, 0, 2), Quaternion.identity * Quaternion.Euler(0, -45, 0));
                 grower.SetDontSaveMe(dontSaveMe);
                 WorldObjectsHandler.InstantiateWorldObject(grower, false);
             }
@@ -170,22 +176,86 @@ namespace FeatTechniciansExile
                 WorldObjectsHandler.InstantiateWorldObject(collector, false);
             }
 
+            logger.LogInfo("  Finding Flower Pot");
+            flower = WorldObjectsHandler.GetWorldObjectViaId(technicianWorldObjectIdStart + 7);
+            if (flower == null)
+            {
+                logger.LogInfo("    Creating the Flower Pot");
+                flower = WorldObjectsHandler.CreateNewWorldObject(GroupsHandler.GetGroupViaId("FlowerPot1"),
+                    technicianWorldObjectIdStart + 7);
+                flower.SetPositionAndRotation(livingPodFloor + new Vector3(-2, 0, 2.25f), Quaternion.identity);
+                flower.SetDontSaveMe(dontSaveMe);
+                WorldObjectsHandler.InstantiateWorldObject(flower, false);
+
+                Inventory inv = InventoriesHandler.GetInventoryById(flower.GetLinkedInventoryId());
+                WorldObject flowerSeed = WorldObjectsHandler.CreateNewWorldObject(GroupsHandler.GetGroupViaId("SeedGold"));
+                if (!inv.AddItem(flowerSeed))
+                {
+                    WorldObjectsHandler.DestroyWorldObject(flowerSeed);
+                }
+            }
+
+            logger.LogInfo("  Finding Chair");
+            chair = WorldObjectsHandler.GetWorldObjectViaId(technicianWorldObjectIdStart + 8);
+            if (chair == null)
+            {
+                logger.LogInfo("    Creating the Chair");
+                chair = WorldObjectsHandler.CreateNewWorldObject(GroupsHandler.GetGroupViaId("Chair1"),
+                    technicianWorldObjectIdStart + 8);
+                chair.SetPositionAndRotation(deskPosition + new Vector3(1f, 0, 2), Quaternion.identity * Quaternion.Euler(0, -45, 0));
+                chair.SetDontSaveMe(dontSaveMe);
+                WorldObjectsHandler.InstantiateWorldObject(chair, false);
+            }
+
+            logger.LogInfo("  Finding Chest");
+            chest = WorldObjectsHandler.GetWorldObjectViaId(technicianWorldObjectIdStart + 9);
+            if (chest == null)
+            {
+                logger.LogInfo("    Creating the Chest");
+                chest = WorldObjectsHandler.CreateNewWorldObject(GroupsHandler.GetGroupViaId("Container1"),
+                    technicianWorldObjectIdStart + 9);
+                chest.SetPositionAndRotation(livingPodFloor + new Vector3(-0.25f, 0, 2.25f), Quaternion.identity * Quaternion.Euler(0, 90, 0));
+                chest.SetDontSaveMe(dontSaveMe);
+                chest.SetText("Technician #221021");
+                WorldObjectsHandler.InstantiateWorldObject(chest, false);
+            }
+
+            logger.LogInfo("  Finding Solar");
+            solar = WorldObjectsHandler.GetWorldObjectViaId(technicianWorldObjectIdStart + 10);
+            if (solar == null)
+            {
+                logger.LogInfo("    Creating the Solar");
+                solar = WorldObjectsHandler.CreateNewWorldObject(GroupsHandler.GetGroupViaId("EnergyGenerator2"),
+                    technicianWorldObjectIdStart + 10);
+                solar.SetPositionAndRotation(livingPodFloor + new Vector3(0, 5, 0), Quaternion.identity);
+                solar.SetDontSaveMe(dontSaveMe);
+                WorldObjectsHandler.InstantiateWorldObject(solar, false);
+            }
+
             logger.LogInfo("  Creating the Technician");
             avatar = TechnicianAvatar.CreateAvatar(Color.white);
             avatar.SetPosition(technicianDropLocation + new Vector3(0, -0.5f, 0), Quaternion.identity * Quaternion.Euler(0, -90, 0));
 
-
             logger.LogInfo("  Limiting object interactions");
-            Destroy(livingPod.GetGameObject().GetComponent<ActionDeconstructible>());
-            Destroy(desk.GetGameObject().GetComponent<ActionDeconstructible>());
-            Destroy(bed.GetGameObject().GetComponent<ActionDeconstructible>());
-            Destroy(screen.GetGameObject().GetComponent<ActionDeconstructible>());
-            Destroy(grower.GetGameObject().GetComponent<ActionDeconstructible>());
-            Destroy(grower.GetGameObject().GetComponent<ActionnableInteractive>());
-            Destroy(grower.GetGameObject().GetComponent<ActionOpenable>());
-            Destroy(collector.GetGameObject().GetComponent<ActionDeconstructible>());
-            Destroy(collector.GetGameObject().GetComponent<ActionnableInteractive>());
-            Destroy(collector.GetGameObject().GetComponent<ActionOpenable>());
+            Destroy(livingPod.GetGameObject().GetComponentInChildren<ActionDeconstructible>());
+            Destroy(desk.GetGameObject().GetComponentInChildren<ActionDeconstructible>());
+            Destroy(bed.GetGameObject().GetComponentInChildren<ActionDeconstructible>());
+            Destroy(screen.GetGameObject().GetComponentInChildren<ActionDeconstructible>());
+            Destroy(grower.GetGameObject().GetComponentInChildren<ActionDeconstructible>());
+            Destroy(grower.GetGameObject().GetComponentInChildren<ActionnableInteractive>());
+            Destroy(grower.GetGameObject().GetComponentInChildren<ActionOpenable>());
+            Destroy(collector.GetGameObject().GetComponentInChildren<ActionDeconstructible>());
+            Destroy(collector.GetGameObject().GetComponentInChildren<ActionnableInteractive>());
+            Destroy(collector.GetGameObject().GetComponentInChildren<ActionOpenable>());
+            Destroy(flower.GetGameObject().GetComponentInChildren<ActionDeconstructible>());
+            Destroy(flower.GetGameObject().GetComponentInChildren<ActionnableInteractive>());
+            Destroy(flower.GetGameObject().GetComponentInChildren<ActionOpenable>());
+            Destroy(chair.GetGameObject().GetComponentInChildren<ActionDeconstructible>());
+            Destroy(chest.GetGameObject().GetComponentInChildren<ActionnableInteractive>());
+            Destroy(chest.GetGameObject().GetComponentInChildren<ActionOpenable>());
+            Destroy(chest.GetGameObject().GetComponentInChildren<ActionDeconstructible>());
+            Destroy(chest.GetGameObject().GetComponentInChildren<ActionOpenUi>());
+            Destroy(solar.GetGameObject().GetComponentInChildren<ActionDeconstructible>());
 
             logger.LogInfo("Done");
         }
