a88f617e70de49548d0ce7f9f116b0ae7bf78108
akarnokd <akarnokd@gmail.com>
2023-02-20 12:16:45 +0100

    Update hungarian labels

--- UITranslationHungarian/Plugin.cs
+++ UITranslationHungarian/Plugin.cs
@@ -12,7 +12,7 @@ using System.Collections;
 
 namespace UITranslationHungarian
 {
-    [BepInPlugin("akarnokd.theplanetcraftermods.uitranslationhungarian", "(UI) Hungarian Translation", "1.0.0.17")]
+    [BepInPlugin("akarnokd.theplanetcraftermods.uitranslationhungarian", "(UI) Hungarian Translation", "1.0.0.18")]
     public class Plugin : BaseUnityPlugin
     {
         const string languageKey = "hungarian";
