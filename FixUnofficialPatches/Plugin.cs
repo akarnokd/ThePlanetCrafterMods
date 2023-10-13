bc89d40bc80c3406c72807f3446a837f5a3ae98e
akarnokd <akarnokd@gmail.com>
2023-10-13 12:57:08 +0200

    More dropdown patches

--- FixUnofficialPatches/Plugin.cs
+++ FixUnofficialPatches/Plugin.cs
@@ -483,5 +483,24 @@ namespace FixUnofficialPatches
         {
             return ___dropdown != null;
         }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(UiDropDownAndHover), nameof(UiDropDownAndHover.SelectOption))]
+        static bool UiDropDownAndHover_SelectOption(TMP_Dropdown ___dropdown)
+        {
+            return ___dropdown != null;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(UiDropDownAndHover), nameof(UiDropDownAndHover.GetIndexOfSelectedItem))]
+        static bool UiDropDownAndHover_GetIndexOfSelectedItem(TMP_Dropdown ___dropdown, ref int __result)
+        {
+            if (___dropdown == null)
+            {
+                __result = -1;
+                return false;
+            }
+            return true;
+        }
     }
 }
