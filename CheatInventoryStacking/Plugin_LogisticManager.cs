fe1cc3c7eaf04331e9de65dda377dc3dbd9a8497
akarnokd <akarnokd@gmail.com>
2024-01-24 19:26:00 +0100

    Stacking: work on drones

--- CheatInventoryStacking/Plugin_LogisticManager.cs
+++ CheatInventoryStacking/Plugin_LogisticManager.cs
@@ -11,7 +11,7 @@ namespace CheatInventoryStacking
     {
         [HarmonyPrefix]
         [HarmonyPatch(typeof(LogisticManager), "SetLogisticTasks")]
-        static bool LogisticManager_SetLogisticTasks_Pre(
+        static bool LogisticManager_SetLogisticTasks(
             NetworkVariable<bool> ____hasLogisticsEnabled,
             Dictionary<int, LogisticTask> ____allLogisticTasks,
             List<MachineDroneStation> ____allDroneStations,
@@ -34,8 +34,8 @@ namespace CheatInventoryStacking
         [HarmonyPrefix]
         [HarmonyPatch(typeof(LogisticManager), "FindDemandForDroneOrDestroyContent")]
         static bool LogisticManager_FindDemandForDroneOrDestroyContent(
-
-            Drone drone
+            Drone drone,
+            List<Inventory> ____demandInventories
         )
         {
             var n = stackSize.Value;
@@ -44,9 +44,46 @@ namespace CheatInventoryStacking
                 return true;
             }
 
-            TODO
+            var droneInv = drone.GetDroneInventory();
+            var items = droneInv.GetInsideWorldObjects();
+
+            if (items.Count == 0)
+            {
+                // We don't let the original method crash either...
+                return false;
+            }
+
+            var wo = items[0];
+            var gr = wo.GetGroup();
+            var gid = gr.id;
+            
+            drone.SetLogisticTask(null);
+
+            foreach (var inv in ____demandInventories)
+            {
+                if (inv.GetLogisticEntity().GetDemandGroups().Contains(gr) 
+                    && !IsFullStackedOfInventory(inv, gid))
+                {
+                    var task = CreateNewTaskForWorldObject(droneInv, inv, wo);
+                    if (task != null)
+                    {
+                        task.SetTaskState(LogisticData.TaskState.ToDemand);
+                        drone.SetLogisticTask(task);
+                        return false;
+                    }
+                }
+            }
+
+            WorldObjectsHandler.Instance.DestroyWorldObject(wo);
 
             return false;
+        }
+
+        static LogisticTask CreateNewTaskForWorldObject(
+            Inventory source, Inventory target, WorldObject worldObject)
+        {
+            TODO
+            throw new NotImplementedException();
         }
     }
 }
