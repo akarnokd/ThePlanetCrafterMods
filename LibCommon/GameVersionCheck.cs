a155055944b441b55eb485a55eb2fec137d7e8d8
akarnokd <akarnokd@gmail.com>
2024-04-02 11:41:12 +0200

    Support v0.9.034

--- LibCommon/GameVersionCheck.cs
+++ LibCommon/GameVersionCheck.cs
@@ -13,7 +13,7 @@ namespace LibCommon
     /// </summary>
     internal class GameVersionCheck
     {
-        const string TargetVersion = "0.9.033";
+        const string TargetVersion = "0.9.034";
 
         static string modName;
 
