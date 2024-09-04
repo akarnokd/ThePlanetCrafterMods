99bd8f271cafc82cc3a7231b580f0e418550b986
akarnokd <akarnokd@gmail.com>
2024-09-04 15:57:54 +0200

    Support 1.300 private

--- LibCommon/GameVersionCheck.cs
+++ LibCommon/GameVersionCheck.cs
@@ -13,7 +13,7 @@ namespace LibCommon
     /// </summary>
     internal class GameVersionCheck
     {
-        const string TargetVersion = "1.212";
+        const string TargetVersion = "1.300";
 
         static string modName;
 
