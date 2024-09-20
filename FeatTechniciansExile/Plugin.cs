caa09d78881fb9f77ddcca5e0e6ec392aeaf6919
akarnokd <akarnokd@gmail.com>
2024-09-20 14:58:28 +0200

    Fix technician position, use default map grid

--- FeatTechniciansExile/Plugin.cs
+++ FeatTechniciansExile/Plugin.cs
@@ -33,7 +33,7 @@ namespace FeatTechniciansExile
         static Dictionary<string, Vector3> technicianDropLocationPerPlanet = new() {
             { "",  new Vector3(1995, 68.8f, 1073) },
             { "Prime",  new Vector3(1995, 68.8f, 1073) },
-            { "Humble", new Vector3(1327, 370.5f, -333) }
+            { "Humble", new Vector3(1327, 347.6f, -333) }
         };
 
         static TechnicianAvatar avatar;
