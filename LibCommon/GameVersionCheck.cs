e70e7dc9de041213cdb6ff0a3d21e60e75893f8a
akarnokd <akarnokd@gmail.com>
2024-10-03 12:01:37 +0200

    Support 1.307

--- LibCommon/GameVersionCheck.cs
+++ LibCommon/GameVersionCheck.cs
@@ -13,7 +13,7 @@ namespace LibCommon
     /// </summary>
     internal class GameVersionCheck
     {
-        const string TargetVersion = "1.306";
+        const string TargetVersion = "1.307";
 
         static string modName;
 
