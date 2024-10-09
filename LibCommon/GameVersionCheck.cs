e3d2d3573bdd9d36395f30c6f5bd9bba50fa9e36
akarnokd <akarnokd@gmail.com>
2024-10-09 12:00:06 +0200

    Release 1.310 main branch

--- LibCommon/GameVersionCheck.cs
+++ LibCommon/GameVersionCheck.cs
@@ -13,7 +13,7 @@ namespace LibCommon
     /// </summary>
     internal class GameVersionCheck
     {
-        const string TargetVersion = "1.309";
+        const string TargetVersion = "1.310";
 
         static string modName;
 
