376f97c68f8cddb0498db0619c68d6e7a423a51c
David Karnok <akarnokd@gmail.com>
2024-10-14 15:31:53 +0200

    Release 1.312 (#117)

--- LibCommon/GameVersionCheck.cs
+++ LibCommon/GameVersionCheck.cs
@@ -13,7 +13,7 @@ namespace LibCommon
     /// </summary>
     internal class GameVersionCheck
     {
-        const string TargetVersion = "1.311";
+        const string TargetVersion = "1.312";
 
         static string modName;
 
