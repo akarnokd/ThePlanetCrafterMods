fc8bbaa15ba98eacd9cc60443cae20c20c4f43ca
akarnokd <akarnokd@gmail.com>
2024-10-17 11:44:10 +0200

    Target 1.315

--- LibCommon/GameVersionCheck.cs
+++ LibCommon/GameVersionCheck.cs
@@ -13,7 +13,7 @@ namespace LibCommon
     /// </summary>
     internal class GameVersionCheck
     {
-        const string TargetVersion = "1.314";
+        const string TargetVersion = "1.315";
 
         static string modName;
 
