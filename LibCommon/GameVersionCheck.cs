fdbd0ef82098e765d546548ba039d6f2e96011b0
akarnokd <akarnokd@gmail.com>
2025-11-12 15:35:52 +0100

    CFNC: adapt to internal game changes; Hun v1.605

--- LibCommon/GameVersionCheck.cs
+++ LibCommon/GameVersionCheck.cs
@@ -13,7 +13,7 @@ namespace LibCommon
     /// </summary>
     internal class GameVersionCheck
     {
-        const string TargetVersion = "1.604";
+        const string TargetVersion = "1.605";
 
         static string modName;
 
