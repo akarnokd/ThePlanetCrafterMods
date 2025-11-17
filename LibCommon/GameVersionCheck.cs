c75227f5bcfbd5bcc32a987507d21e20a082d489
akarnokd <akarnokd@gmail.com>
2025-11-17 13:21:58 +0100

    Release v1.607 - version bumps only

--- LibCommon/GameVersionCheck.cs
+++ LibCommon/GameVersionCheck.cs
@@ -13,7 +13,7 @@ namespace LibCommon
     /// </summary>
     internal class GameVersionCheck
     {
-        const string TargetVersion = "1.606";
+        const string TargetVersion = "1.607";
 
         static string modName;
 
